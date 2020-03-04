68bf2a89f8b30dfeb9d696905ff8cc8832b6d61b
stcote <stcote@gmail.com>
2020-03-04 16:24:40 -0500

    Add networking

--- FpWindow.cpp
+++ FpWindow.cpp
@@ -2,12 +2,16 @@
 #include "ui_FpWindow.h"
 #include <QLocalServer>
 #include <QLocalSocket>
+#include <QTcpSocket>
 #include <QDate>
 #include <QDebug>
+#include <QTimer>
 
 const QString FP_PIPE_NAME = "FP_Pipe";
 
 const quint16 SCALE_PORT = 29456;
+const QString SCALE_ADDR = "10.0.0.1";
+
 
 const int NAME_MAX = 127;
 
@@ -21,6 +25,7 @@ typedef struct
 
 const int CHECKIN_SIZE = sizeof( t_CheckIn );
 
+const int CONNECT_TIMEOUT_MS = 1000;
 
 //*****************************************************************************
 //*****************************************************************************
@@ -35,6 +40,10 @@ FpWindow::FpWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    attemptingConnect_ = false;
+    isConnected_       = false;
+    tmOutCnt_          = 0;
+
     //*** create new checkin server ***
     svr_ = new QLocalServer( this );
 
@@ -48,22 +57,37 @@ FpWindow::FpWindow(QWidget *parent) :
         ui->textOut->append( "Error listening for connection!!!" );
     }
 
+    //*** create the icons we need ***
     goodIcon_ = QIcon(":/images/good.png");
     badIcon_  = QIcon(":/images/bad.png");
 
+    //*** set up the tray interface ***
     createActions();
     createTrayIcon();
 
+    //*** handle activation ***
     connect( trayIcon_, &QSystemTrayIcon::activated, this, &FpWindow::iconActivated);
 
-    showGoodIcon();
-    ui->statusLbl->setPixmap( QPixmap( ":/images/good.png" ) );
+    //*** show bad icon until we connect ***
+    showBadIcon();
 
+    //*** show the tray (icon) ***
     trayIcon_->show();
 
+    //*** set icon for the program ***
+    setWindowIcon( goodIcon_ );
+
+    //*** start minimized (to tray) ***
     showMinimized();
 
+    //*** in one second, hide it - can't do right away ***
+    QTimer::singleShot( 1000, [=]() { this->hide(); } );
+
+    //*** Title for the application window ***
     setWindowTitle( "Checkin Server" );
+
+    //*** start trying to connect to scale server ***
+    setupNetworking();
 }
 
 
@@ -76,6 +100,13 @@ FpWindow::FpWindow(QWidget *parent) :
 FpWindow::~FpWindow()
 {
     delete ui;
+
+    delete trayIcon_;
+    delete trayIconMenu_;
+
+    delete svr_;
+    delete scaleSock_;
+
 }
 
 
@@ -166,6 +197,12 @@ t_CheckIn ci;
                 .arg( ci.key ).arg( ci.name ).arg( ci.numItems ).arg( day );
 
         ui->textOut->append( buf );
+
+        //*** send to scale server if connected ***
+        if ( isConnected_ )
+        {
+            scaleSock_->write( (const char*)&ci, CHECKIN_SIZE );
+        }
     }
     else
     {
@@ -232,4 +269,137 @@ void FpWindow::showGoodIcon()
 void FpWindow::showBadIcon()
 {
     trayIcon_->setIcon( badIcon_ );
+}
+
+
+//*****************************************************************************
+//*****************************************************************************
+/**
+ * @brief FpWindow::setupNetworking
+ */
+//*****************************************************************************
+void FpWindow::setupNetworking()
+{
+    scaleSock_ = new QTcpSocket( this );
+
+    //*** set up connection endpoint ***
+    scalePort_ = SCALE_PORT;
+    scaleAddr_.setAddress( SCALE_ADDR );
+
+    connect(scaleSock_, SIGNAL(connected()), SLOT(handleTcpConnected()));
+    connect(scaleSock_, SIGNAL(disconnected()), SLOT(handleTcpDisconnected()));
+    connect(scaleSock_, SIGNAL(error(QAbstractSocket::SocketError)), SLOT(handleTcpError(QAbstractSocket::SocketError)));
+
+    //*** start attempting to connect ***
+    attemptReconnect();
+}
+
+
+//********************************************************************************
+//********************************************************************************
+/**
+ * Closes the socket if it is open and starts an asynchronous connection attempt. On timeout, checkConnectionFailed() will be called.
+ *
+ * If we are already trying to connect, this function does nothing.
+ */
+//********************************************************************************
+void FpWindow::attemptReconnect()
+{
+    // only continue if we're not already connecting
+    if (!attemptingConnect_)
+    {
+       if (scaleSock_->isOpen())
+       {
+          scaleSock_->close();
+       }
+
+       // attempt the connect. we will check later if it succeeded.
+       isConnected_ = false;
+       attemptingConnect_ = true;
+       scaleSock_->connectToHost( scaleAddr_, scalePort_, QAbstractSocket::ReadWrite);
+
+
+       // after the timeout, check if we have connected. If the connection succeeds, connected() will handle going forward and this timeout will do nothing
+       QTimer::singleShot(CONNECT_TIMEOUT_MS, this, SLOT(checkConnectionFailed()));
+    }
+}
+
+
+//********************************************************************************
+//********************************************************************************
+/**
+ * Called after the timeout is reached after attemptReconnect() is called. If the socket is still closed, it is assumed that an error occurred
+ * and attempts a reconnect. If the socket is open, nothing more happens.
+ */
+//********************************************************************************
+void FpWindow::checkConnectionFailed()
+{
+    if (!isConnected_)
+    {
+       // connection failed, try again
+       tmOutCnt_++;
+       if ( tmOutCnt_ % 60 == 0 )
+       {
+          ui->textOut->append( "Unable to connect, retrying..." );
+       }
+
+       attemptingConnect_ = false;
+       attemptReconnect();
+    }
+}
+
+
+//********************************************************************************
+//********************************************************************************
+/**
+ * Occurs when the tcp socket connects. Sends each message that has been set.
+ */
+//********************************************************************************
+void FpWindow::handleTcpConnected()
+{
+    ui->textOut->append( "Connected..." );
+
+    isConnected_ = true;
+    attemptingConnect_ = false;
+    tmOutCnt_ = 0;
+
+    showGoodIcon();
+}
+
+
+//********************************************************************************
+//********************************************************************************
+/**
+ * Occurs when the tcp socket disconnects. Attempts a reconnect.
+ */
+//********************************************************************************
+void FpWindow::handleTcpDisconnected()
+{
+    ui->textOut->append( "Server disconnected, reconnecting..." );
+
+    attemptingConnect_ = false;
+    attemptReconnect();
+
+    showBadIcon();
+}
+
+
+//********************************************************************************
+//********************************************************************************
+/**
+ * Occurs if an error is thrown by the TCP socket. Logs the error.
+ *
+ * This does not attempt a reconnect as many of the error types happen immediately on connectToHost and would result in an infinite loop.
+ * If an error occurs and the socket disconnects, a reconnect will be attempted the next time setInfo is called.
+ *
+ * @param e  The type of error that occurred
+ */
+//********************************************************************************
+void FpWindow::handleTcpError( QAbstractSocket::SocketError e )
+{
+    if ( e != QAbstractSocket::ConnectionRefusedError )
+    {
+        ui->textOut->append( "TCP Error!!!" );
+        qDebug() << "Error (" << e << "): " << scaleSock_->errorString();
+    }
 }
--- FpWindow.h
+++ FpWindow.h
@@ -3,13 +3,15 @@
 
 #include <QMainWindow>
 #include <QSystemTrayIcon>
+#include <QHostAddress>
+#include <QTimer>
 
 namespace Ui {
 class FpWindow;
 }
 
 class QLocalServer;
-
+class QTcpSocket;
 
 
 //*****************************************************************************
@@ -34,6 +36,58 @@ private slots:
     void handleConnection();
     void handleRead();
 
+    //********************************************************************************
+    //********************************************************************************
+    /**
+     * Closes the socket if it is open and starts an asynchronous connection attempt. On timeout, checkConnectionFailed() will be called.
+     *
+     * If we are already trying to connect, this function does nothing.
+     */
+    //********************************************************************************
+    void attemptReconnect();
+
+
+    //********************************************************************************
+    //********************************************************************************
+    /**
+     * Called after the timeout is reached after attemptReconnect() is called. If the socket is still closed, it is assumed that an error occurred
+     * and attempts a reconnect. If the socket is open, nothing more happens.
+     */
+    //********************************************************************************
+    void checkConnectionFailed();
+
+
+    //********************************************************************************
+    //********************************************************************************
+    /**
+     * Occurs when the tcp socket connects. Sends each message that has been set.
+     */
+    //********************************************************************************
+    void handleTcpConnected();
+
+
+    //********************************************************************************
+    //********************************************************************************
+    /**
+     * Occurs when the tcp socket disconnects. Attempts a reconnect.
+     */
+    //********************************************************************************
+    void handleTcpDisconnected();
+
+
+    //********************************************************************************
+    //********************************************************************************
+    /**
+     * Occurs if an error is thrown by the TCP socket. Logs the error.
+     *
+     * This does not attempt a reconnect as many of the error types happen immediately on connectToHost and would result in an infinite loop.
+     * If an error occurs and the socket disconnects, a reconnect will be attempted the next time setInfo is called.
+     *
+     * @param e  The type of error that occurred
+     */
+    //********************************************************************************
+    void handleTcpError( QAbstractSocket::SocketError e );
+
 
 private:
 
@@ -43,18 +97,35 @@ private:
     void showGoodIcon();
     void showBadIcon();
 
+    void setupNetworking();
+
 
     Ui::FpWindow *ui;
 
+    //*** Windows 'Named Pipe' server ***
     QLocalServer *svr_;
 
+    //*** client socket to talk to scale server ***
+    QTcpSocket *scaleSock_;
+
+    QHostAddress scaleAddr_;
+    quint16      scalePort_;
+    bool attemptingConnect_;   // True if we are attempting to connect, false if we are connected or disconnected
+    bool isConnected_;         // True if we are connected to the TCP server
+    int tmOutCnt_;
+
+    //*** menu actions ***
     QAction *showWeightAction_;
     QAction *showAction_;
     QAction *hideAction_;
 
+    //*** tray icon ***
     QSystemTrayIcon *trayIcon_;
+
+    //*** menu for tray icon ***
     QMenu *trayIconMenu_;
 
+    //*** Tray icons ***
     QIcon goodIcon_;
     QIcon badIcon_;
 
